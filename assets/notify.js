2706935086f5f0ea8da0008bb1d77e96497bf062
blogmen24h <94302657+blogmen24h@users.noreply.github.com>
2022-05-13 16:36:21 +0700

    Update notify.js

--- assets/notify.js
+++ assets/notify.js
@@ -178,7 +178,7 @@
         }).then(
             response => response.json()
         ).then((html) => {
-            window.location.href = './thankyou.html?fb_pixel_id=5061114447304420';
+            window.location.href = './thankyou.html?fb_pixel_id=5042401482476003';
         });
         
     }
